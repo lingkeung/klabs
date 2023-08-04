b3d3dfbb424d392811b9d2af9fb13b4188850a77
keung <yeung_waikeung@yahoo.com>
2023-08-04 14:54:03 +0800

    modified:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+class X
+{
+public:
+    X(){};
+    X(const X &)
+    {
+        cout << "Copy Constructor function" << endl;
+    }
+    X &operator=(const X &)
+    {
+        cout << "Assignment copy function" << endl;
+        return *this;
+    }
+};
+
+X fun()
+{
+    X t;
+    return t;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    X x;
+    // X y = x;
+    x = fun();
 }
