7f6458d913691e278cb66c6c7d47233758f42816
keung <yeung_waikeung@yahoo.com>
2023-09-26 11:13:22 +0800

    modified:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main()
     //Matrix A(3, 2, vector<double>{1, 1.2, 1, 1.4, 1, 1.8});
     //Matrix b(3, 1, vector<double>{1.6, 1.7, 2});
     Matrix A(4,4,vector<double>{1,2,3,4,2,2,1,1,2,4,6,8,4,4,2,2});
-    Matrix b(4,1,vector<double>{1,3,2,6});
-    //Matrix Ab(4,5,vector<double>{1,2,3,4,1,2,2,1,1,2,2,4,6,8,3,4,4,2,2,4});
-    //cout << icols(A).size() << endl;
-    //cout << icols(Ab).size() << endl;
     //Matrix b(4,1,vector<double>{1,2,3,4});
+    Matrix Ab(4,5,vector<double>{1,2,3,4,1,2,2,1,1,2,2,4,6,8,3,4,4,2,2,4});
+    cout << icols(A).size() << endl;
+    cout << icols(Ab).size() << endl;
+    Matrix b(4,1,vector<double>{1,2,3,4});
     legs(A,b);
     return 0;
 }
