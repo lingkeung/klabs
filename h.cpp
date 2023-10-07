c1696b257428d631d9956fcb747c70e2740740c2
keung <yeung_waikeung@yahoo.com>
2023-10-07 12:11:57 +0800

    modified:   Qr.cpp     new file:   h.cpp 	modified:   klabs 	modified:   main 	modified:   main.cpp

---
+++ h.cpp
@@ -0,0 +1,1 @@
+#include "leqs.h"
--- main.cpp
+++ main.cpp
@@ -1,26 +1,70 @@
-#include "leqs.h"
-// #include <iostream>
+#include "eig.h"
+#include "eigen.h"
+#include "svd.h"
+#include "svdc.h"
+#include "qr.h"
+#include "qrc.h"
+
+// #include "cmatrix.h"
+//  #include <iostream>
+
+void francisOS(Matrix &H)
+{
+    int n = H.getN();
+    int m = n - 1;
+    double s, t, x, y, z;
+    s = H(m, m) + H(n, n);
+    t = H(m, m) * H(n, n) - H(m, n) * H(n, m);
+    x = H(1, 1) * H(1, 1) + H(1, 2) * H(2, 1) - s * H(1, 1) + t + 0.00001;
+    y = H(2, 1) * (H(1, 1) + H(2, 2) - s) + 0.00001;
+    z = H(2, 1) * H(3, 2);
+    cout << x << " " << y << " " << z << endl; // debug
+    for (int k = 0; k <= n - 3; k++)
+    {
+        Matrix xyz(3, 1, vector<double>{x, y, z});
+        Matrix v;
+        double beta;
+        house(xyz, v, beta);
+        cout << "xyz =" << endl;           // debug
+        xyz.print();                       // debug
+        cout << "v = " << endl;            // debug
+        v.print();                         // debug
+        cout << "beta = " << beta << endl; // debug
+        int q = (k > 1) ? k : 1;           // k>=1 changed to k>1 // debug
+        H.setblk(k + 1, q, (identity(3) - beta * v * v.transpose()) * H(k + 1, k + 3, q, n));
+        int r = (k + 4 <= n) ? k + 4 : n;
+        H.setblk(1, k + 1, H(1, r, k + 1, k + 3) * (identity(3) - beta * v * v.transpose()));
+        x = H(k + 2, k + 1);
+        y = H(k + 3, k + 1);
+        if (k < n - 3)
+        {
+            z = H(k + 4, k + 1);
+        }
+    }
+
+    Matrix xy(2, 1, vector<double>{x, y});
+    Matrix v;
+    double beta;
+    house(xy, v, beta);
+    H.setblk(n - 1, n - 2, (identity(2) - beta * v * v.transpose()) * H(n - 1, n, n - 2, n));
+    H.setblk(1, n - 1, H(1, n, n - 1, n) * (identity(2) - beta * v * v.transpose()));
+    // return H;
+}
 
 int main()
 {
-    Matrix A(4, 5, vector<double>{1, 0, 1, -1, -3, 1, 2, -1, 0, -1, 4, 6, -2, -4, 3, 2, -2, 4, -7, 4});
-    Matrix b(4, 1, vector<double>{-2, 1, 7, 1});
-    //  Matrix b(4,1);
-    // Matrix A(4,4,vector<double>{1,0,-3,-6,2,-5,1,1,0,-1,2,2,1,-7,4,6});
-    // Matrix b(4,1,vector<double>{9,8,-5,0});
-    // Matrix A(3, 2, vector<double>{1, 1.2, 1, 1.4, 1, 1.8});
-    // Matrix b(3, 1, vector<double>{1.6, 1.7, 2});
-    // Matrix A(4, 4, vector<double>{1, 2, 3, 4, 2, 2, 1, 1, 2, 4, 6, 8, 4, 4, 2, 2});
-    // Matrix b(4, 1, vector<double>{1, 2, 3, 4});
-    // Matrix Ab(4,5,vector<double>{1,2,3,4,1,2,2,1,1,2,2,4,6,8,3,4,4,2,2,4});
-    // cout << icols2(A).size() << endl;
-    // cout << icols(A).size() << endl;
-    // Matrix b(4,1,vector<double>{1,2,3,4});
-    // legs(A, b);
-    A.print();
-    nullspace(A).print();
-    xp(A, b).print();
-    (A * xp(A, b)).print();
-    b.print();
+    // Matrix A(3, 3, {1,1.5,0.5,1.5,7,5.5,0.5,5.5,3});
+    Matrix A(3, 3, {3.0, -1, 0.00000, -1, 3, -1, 0.00000, -1, 3.0});
+    Matrix B = A;
+    Eigen e(A);
+    cMatrix Lamda = e.getLamda();
+    Lamda.print();
+    SVD s(B);
+    Matrix Sigma = s.getSigma();
+    Matrix U = s.getU();
+    Matrix VT = s.getVT();
+    (U * Sigma * VT).print();
+    Sigma.print();
+
     return 0;
 }
